aa931d5f543f304c2ae7d55ea0ca7743342b4f73
ZZP-DMU <3571669089@qq.com>
2023-05-18 10:20:41 +0800

    add the init mode to help users add their own appid and seckey

---
+++ init.py
@@ -0,0 +1,42 @@
+import os 
+import json
+
+class app_info_mode():
+    def __init__(self):
+        self.path = os.getcwd()
+        #get the appid&seckey;if none set before,then input
+        #and store in info.json,if stored in info.json,then 
+        #read it 
+        fd = open(self.path+"/info.json","r")
+        info_dict = json.load(fd)
+        info_private_appid = info_dict["private"]["appid"]
+        info_private_seckey = info_dict["private"]["seckey"]
+        if info_private_appid == info_private_seckey:
+            self.appid = input("set the appid:")
+            self.seckey = input("set the seckey:")
+            fd.close()
+
+            info_dict["private"]["appid"]=self.appid
+            info_dict["private"]["seckey"]=self.seckey
+
+            #make the str that will write into the info.json
+            #file a easy format to read
+            info_str = str()
+            for i in json.dumps(info_dict):
+                info_str = info_str + i
+                if i == ',':
+                    info_str = info_str+'\n'
+
+            #wirte the str into the info.json
+            fd = open(self.path+"/info.json","w")
+            fd.write(info_str)
+            fd.close()
+        else:
+            self.appid = info_private_appid
+            self.seckey = info_private_seckey
+            fd.close()
+if __name__ == "__main__":
+    app_info = app_info_mode()
+    print(app_info.appid,app_info.seckey)
+else:
+    app_info = app_info_mode
--- translate.py
+++ translate.py
@@ -3,12 +3,15 @@ import hashlib
 import urllib.parse
 import random
 import json
+import init
 
 def translate(string,fr_lang = 'en',to_lang = 'zh'):
     # 所需的参数
     url = 'https://api.fanyi.baidu.com/api/trans/vip/translate'
-    appid = '20221023001410967'
-    secretKey = 'r0plAW8DBJSTzb1ycMQY'
+    
+    appid = init.app_info.appid
+    secretKey = init.app_info.seckey
+    #print(appid,secretKey)
     fromLang = fr_lang
     toLang = to_lang
     salt = random.randint(32768, 65536)
@@ -32,3 +35,6 @@ def translate(string,fr_lang = 'en',to_lang = 'zh'):
             #print(res_dict['trans_result'][0]['dst'])
         except Exception as e:
             print(f'{e}\n{res_dict}')
+
+if __name__ == "__main__":
+    print(translate("hello",'en','zh'))
